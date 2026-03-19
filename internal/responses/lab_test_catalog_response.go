f6b6801ea298064cdf63805f853915039e249651
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-19 16:43:49 +0530

    Fix: Add ID field to LabTestCatalogResponse struct and its initialization

--- internal/responses/lab_test_catalog_response.go
+++ internal/responses/lab_test_catalog_response.go
@@ -3,6 +3,7 @@ package responses
 import "go-echo-starter/internal/models"
 
 type LabTestCatalogResponse struct {
+	ID           int64   `json:"id" example:"1"`
 	Name         string  `json:"name" example:"Blood Test"`
 	DefaultPrice float64 `json:"default_price" example:"7500"`
 }
@@ -12,6 +13,7 @@ func NewLabTestCatalogResponse(labtestcatalogs []models.LabTestCatalog) *[]LabTe
 
 	for i := range labtestcatalogs {
 		labtestcatalogResponse = append(labtestcatalogResponse, LabTestCatalogResponse{
+			ID:           labtestcatalogs[i].ID,
 			Name:         labtestcatalogs[i].Name,
 			DefaultPrice: labtestcatalogs[i].DefaultPrice,
 		})
