81c660a2e9d259b9a9e0549857811ed766dedc88
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-19 16:43:43 +0530

    Fix: Add ID field to DrugCatalogResponse struct and its initialization

--- internal/responses/drug_catalog_response.go
+++ internal/responses/drug_catalog_response.go
@@ -3,6 +3,7 @@ package responses
 import "go-echo-starter/internal/models"
 
 type DrugCatalogResponse struct {
+	ID           int64   `json:"id" example:"1"`
 	Name         string  `json:"name" example:"Dupixent"`
 	DefaultPrice float64 `json:"default_price" example:"1200"`
 }
@@ -12,6 +13,7 @@ func NewDrugCatalogResponse(drugcatalogs []models.DrugCatalog) *[]DrugCatalogRes
 
 	for i := range drugcatalogs {
 		drugcatalogResponse = append(drugcatalogResponse, DrugCatalogResponse{
+			ID:           drugcatalogs[i].ID,
 			Name:         drugcatalogs[i].Name,
 			DefaultPrice: drugcatalogs[i].DefaultPrice,
 		})
