15e7fb6e36bc9daa332cd44fb2ce52e02116a903
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-21 11:51:25 +0530

    Fix: Rename GetLabTestCatalogs to List for consistency; update Create method parameter and refactor variable names for clarity

--- internal/repositories/lab_test_catalog.go
+++ internal/repositories/lab_test_catalog.go
@@ -18,8 +18,8 @@ func NewLabTestCatalogRepository(db *gorm.DB) *LabTestCatalogRepository {
 	return &LabTestCatalogRepository{db: db}
 }
 
-func (r *LabTestCatalogRepository) Create(ctx context.Context, labtestcatalog *models.LabTestCatalog) error {
-	if err := r.db.WithContext(ctx).Create(labtestcatalog).Error; err != nil {
+func (r *LabTestCatalogRepository) Create(ctx context.Context, catalog *models.LabTestCatalog) error {
+	if err := r.db.WithContext(ctx).Create(catalog).Error; err != nil {
 		return fmt.Errorf("execute insert labtestcatalog query: %w", err)
 	}
 
@@ -27,13 +27,13 @@ func (r *LabTestCatalogRepository) Create(ctx context.Context, labtestcatalog *m
 }
 
 // In repositories/labtestcatalog.go
-func (r *LabTestCatalogRepository) GetLabTestCatalogs(ctx context.Context) ([]models.LabTestCatalog, error) {
-	var labtestcatalogs []models.LabTestCatalog
+func (r *LabTestCatalogRepository) List(ctx context.Context) ([]models.LabTestCatalog, error) {
+	var catalogs []models.LabTestCatalog
 
 	// Log the query being executed
 	slog.Info("Executing GetLabTestCatalogs query")
 
-	result := r.db.WithContext(ctx).Find(&labtestcatalogs)
+	result := r.db.WithContext(ctx).Find(&catalogs)
 
 	// Log the result
 	slog.Info("GetLabTestCatalogs result",
@@ -45,33 +45,42 @@ func (r *LabTestCatalogRepository) GetLabTestCatalogs(ctx context.Context) ([]mo
 	}
 
 	// Log the actual labtestcatalogs found
-	slog.Info("LabTestCatalogs found", "count", len(labtestcatalogs))
+	slog.Info("LabTestCatalogs found", "count", len(catalogs))
 
-	return labtestcatalogs, nil
+	return catalogs, nil
 }
 
-func (r *LabTestCatalogRepository) GetLabTestCatalog(ctx context.Context, id uint) (models.LabTestCatalog, error) {
-	var labtestcatalog models.LabTestCatalog
-	err := r.db.WithContext(ctx).Where("id = ?", id).Take(&labtestcatalog).Error
+func (r *LabTestCatalogRepository) ListPaginated(pagination Pagination[models.LabTestCatalog]) (*Pagination[models.LabTestCatalog], error) {
+	var catalogs []models.LabTestCatalog
+
+	r.db.Scopes(paginate(catalogs, &pagination, r.db)).Find(&catalogs)
+	pagination.Data = catalogs
+
+	return &pagination, nil
+}
+
+func (r *LabTestCatalogRepository) Get(ctx context.Context, id uint) (models.LabTestCatalog, error) {
+	var catalog models.LabTestCatalog
+	err := r.db.WithContext(ctx).Where("id = ?", id).Take(&catalog).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return models.LabTestCatalog{}, errors.Join(models.ErrLabTestCatalogNotFound, err)
 	} else if err != nil {
 		return models.LabTestCatalog{}, fmt.Errorf("execute select labtestcatalog by id query: %w", err)
 	}
 
-	return labtestcatalog, nil
+	return catalog, nil
 }
 
-func (r *LabTestCatalogRepository) Update(ctx context.Context, labtestcatalog *models.LabTestCatalog) error {
-	if err := r.db.WithContext(ctx).Save(labtestcatalog).Error; err != nil {
+func (r *LabTestCatalogRepository) Update(ctx context.Context, catalog *models.LabTestCatalog) error {
+	if err := r.db.WithContext(ctx).Save(catalog).Error; err != nil {
 		return fmt.Errorf("execute update labtestcatalog query: %w", err)
 	}
 
 	return nil
 }
 
-func (r *LabTestCatalogRepository) Delete(ctx context.Context, labtestcatalog *models.LabTestCatalog) error {
-	if err := r.db.WithContext(ctx).Delete(labtestcatalog).Error; err != nil {
+func (r *LabTestCatalogRepository) Delete(ctx context.Context, catalog *models.LabTestCatalog) error {
+	if err := r.db.WithContext(ctx).Delete(catalog).Error; err != nil {
 		return fmt.Errorf("execute delete labtestcatalog query: %w", err)
 	}
 
