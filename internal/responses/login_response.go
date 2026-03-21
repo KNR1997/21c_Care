409d73f2c44ef23e5364aba659e0603d391077e7
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-21 11:52:34 +0530

    Fix: Update LoginResponse struct to include Permissions and Role fields; modify NewLoginResponse function signature accordingly

--- internal/responses/login_response.go
+++ internal/responses/login_response.go
@@ -1,15 +1,19 @@
 package responses
 
 type LoginResponse struct {
-	AccessToken  string `json:"accessToken"`
-	RefreshToken string `json:"refreshToken"`
-	Exp          int64  `json:"exp"`
+	AccessToken  string    `json:"accessToken"`
+	RefreshToken string    `json:"refreshToken"`
+	Exp          int64     `json:"exp"`
+	Permissions  [1]string `json:"permissions"`
+	Role         string    `json:"role"`
 }
 
-func NewLoginResponse(token, refreshToken string, exp int64) *LoginResponse {
+func NewLoginResponse(token, refreshToken string, exp int64, permissions [1]string, role string) *LoginResponse {
 	return &LoginResponse{
 		AccessToken:  token,
 		RefreshToken: refreshToken,
 		Exp:          exp,
+		Permissions:  permissions,
+		Role:         role,
 	}
 }
